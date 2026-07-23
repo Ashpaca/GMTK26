522557ba90d5f18ddf555c468d02a384c8529aed
Ashpaca <ashley.ashley.harrison@gmail.com>
2026-07-23 00:43:38 -0700

    added empty shelf and shelf items which can be toggled

--- res/player/player.gd
+++ res/player/player.gd
@@ -5,17 +5,23 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 @onready var animation_player: AnimationPlayer = $Visuals/AnimationPlayer
 @onready var visuals: Node3D = $Visuals
+@onready var interaction_zone: Area3D = $Visuals/InteractionZone
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		for body in interaction_zone.get_overlapping_bodies():
+			if body is Shelf:
+				var shelf : Shelf = body
+				if not shelf.is_stocked:
+					shelf.stock_shelf()
+					break
+	
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
