7e7ff7f9df5238c5f31121d5f6f99882de5542ba
Ashpaca <ashley.ashley.harrison@gmail.com>
2026-07-23 19:19:25 -0700

    added event bus with test signal

---
+++ res/globals/event_bus.gd
@@ -0,0 +1,7 @@
+extends Node
+
+@warning_ignore("unused_signal")
+
+# emitted by: game_state
+# connected to: shelf
+signal test_signal
--- res/testing_things/test_label.gd
+++ res/testing_things/test_label.gd
@@ -1,6 +1,5 @@
 extends Label
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	text = str(round(GameState.time_left))
