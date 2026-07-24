64b91d37cbb01f55e54f5a22ff5a92d82863e174
Ashpaca <ashley.ashley.harrison@gmail.com>
2026-07-24 02:39:41 -0700

    changed hitboxes to stop pathing issues

--- res/npcs/shopper.gd
+++ res/npcs/shopper.gd
@@ -5,6 +5,13 @@ const SPEED = 1
 
 
 func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 	navigation_agent_3d.target_position = Vector3(-5.3, 0, 3.3)
-	velocity = (navigation_agent_3d.get_next_path_position() - global_position) * SPEED
+	var next_point : Vector3 = (navigation_agent_3d.get_next_path_position() - global_position) * SPEED
+	navigation_agent_3d.velocity = next_point
 	move_and_slide()
+
+
+func _on_navigation_agent_3d_velocity_computed(safe_velocity: Vector3) -> void:
+	velocity = safe_velocity.normalized() * SPEED
