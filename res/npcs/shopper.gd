c9cd1decbd5c3798bef3cf32c4a68e1ed01bf39e
Ashpaca <ashley.ashley.harrison@gmail.com>
2026-07-24 11:15:33 -0700

    added states to the shopper and reworked a few things

--- res/npcs/shopper.gd
+++ res/npcs/shopper.gd
@@ -1,12 +1,19 @@
 extends CharacterBody3D
 
+enum State{
+	WALK,
+	GRAB,
+	WAIT,
+	DEAD
+}
+
 const SPEED : float = 1
 const DEATH_OFFSET : Vector3 = Vector3(0, 0, .35)
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 @onready var animation_player: AnimationPlayer = $Visuals/AnimationPlayer
 @onready var visuals: Node3D = $Visuals
-
-var is_dead : bool
+var current_state : State = State.WALK
+var has_item : bool
 
 
 func _physics_process(delta: float) -> void:
@@ -14,7 +21,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 		move_and_slide()
 		return
-	if is_dead:
+	if current_state == State.DEAD:
 		return
 	navigation_agent_3d.target_position = Vector3(-5.3, 0, 3.3)
 	var next_point : Vector3 = (navigation_agent_3d.get_next_path_position() - global_position) * SPEED
@@ -24,7 +31,10 @@ func _physics_process(delta: float) -> void:
 		animation_player.play("idle")
 	else:
 		animation_player.play("walk")
-		visuals.look_at(global_position - velocity)
+		
+		var goal_rotation : float = -visuals.basis.x.angle_to(velocity)
+		visuals.rotation.y = rotate_toward(visuals.rotation.y, goal_rotation, delta*10)
+		#visuals.look_at(global_position - velocity)
 	move_and_slide()
 
 
@@ -33,11 +43,11 @@ func _on_navigation_agent_3d_velocity_computed(safe_velocity: Vector3) -> void:
 
 
 func _on_player_detector_body_entered(body: Node3D) -> void:
-	if is_dead:
+	if current_state == State.DEAD:
 		return
 	if body is Player:
 		var player : Player = body
 		if player.is_running():
 			animation_player.play("die")
 			visuals.position = DEATH_OFFSET.rotated(Vector3.UP, visuals.rotation.y)
-			is_dead = true
+			current_state = State.DEAD
