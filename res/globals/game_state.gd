797af56d6066679503de83dd21409d84e79b89e9
Ashpaca <ashley.ashley.harrison@gmail.com>
2026-07-23 18:42:49 -0700

    added game state and a timer

---
+++ res/globals/game_state.gd
@@ -0,0 +1,19 @@
+extends Node
+
+enum State{
+	MENU,
+	PLAY,
+	PAUSE,
+	END
+}
+
+var current_state : State = State.PLAY # will be menu later
+var time_left : float = 60.0
+
+func is_playing() -> bool:
+	return current_state == State.PLAY
+
+
+func _process(delta: float) -> void:
+	if is_playing():
+		time_left -= delta
