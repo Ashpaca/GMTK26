3f957076e012f78653cba8f53e20f81759d6a8ab
Ashpaca <ashley.ashley.harrison@gmail.com>
2026-07-25 04:38:44 -0700

    removed warnings

--- src/ui/start_menu_bg_scroll_speed.gd
+++ src/ui/start_menu_bg_scroll_speed.gd
@@ -5,8 +5,3 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.material.set_shader_parameter("scroll_speed", scroll_speed)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
